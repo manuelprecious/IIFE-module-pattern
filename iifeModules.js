2c57c07bf1fa9b6fbab03558e10276d86c2719a4
Manuel-Smith <riddicksmith37@gmail.com>
2022-11-01 17:30:47 +0300

    Learning about first class functions in javascript

--- iifeModules.js
+++ iifeModules.js
@@ -1,4 +1,19 @@
 // Declaring an immediately invoked function expression.
 var life = (function () {
-    alert("Hello, cranelux.com")
-}());
+    console.log(`Hello Joe, My name is John Lenon!`)
+})();
+
+
+
+// Declaring first class functions
+/**
+ * A programming language with first class functions is one whose functions have the ability to be
+ * treated like normal variable. That means, they can be passed as parameters, they can be return from 
+ * another function, or  they can be assigned to a variable.
+ * These functions can 
+ */
+
+const sayHello = ()=>{
+    console.log("Hello. My name is Mark")
+};
+sayHello();
